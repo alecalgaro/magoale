ac1914a786287ae4d44153401af8f65403f2abb1
alecalgaro <59174208+alecalgaro@users.noreply.github.com>
2020-06-15 19:50:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var m = document.querySelector("a-marker")
+m.addEventListener("markerFound", (e) => {
+    console.log("found")
+    var v = document.querySelector('#mivideo').play();
+
+
+})
+m.addEventListener("markerLost", (e) => {
+    console.log("lost")
+    var v = document.querySelector('#mivideo').pause();
+
+})
+
+
+document.getElementById("btn1").addEventListener("click", (e) => {
+
+    var v = document.querySelector('#mivideo').muted = false;
+})
+document.getElementById("btn2").addEventListener("click", (e) => {
+
+    var v = document.querySelector('#mivideo').muted = true;
+})
+document.getElementById("btn3").addEventListener("click", (e) => {
+
+    var v = document.querySelector('#mivideo').pause();
+})
+document.getElementById("btn4").addEventListener("click", (e) => {
+
+    var v = document.querySelector('#mivideo').play();
+})
+
+
